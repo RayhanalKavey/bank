1345612c2c526ad130bd8ec45b368990d0db8b57
RayhanalKavey <rayhanalkavey@gmail.com>
2022-08-16 05:35:15 +0600

    add the event handler to the login button and check whether the button gives us the result of proper email and password input.

---
+++ js/login.js
@@ -0,0 +1,21 @@
+"use strict";
+// Get Elements
+const loginButton = document.getElementById("btn-login");
+const emailField = document.getElementById("user-email");
+const passwordField = document.getElementById("user-password");
+// Event Listener
+loginButton.addEventListener("click", function () {
+  const emailTyped = emailField.value;
+  console.log(emailTyped);
+  const passwordTyped = passwordField.value;
+  console.log(passwordTyped);
+  // Danger: Do not varify email password on the client site like this. This is only for learning purpose. We usually send it to the server site.
+  if (
+    emailTyped.toLowerCase() === "personal@bank.com" &&
+    passwordTyped === "getMoney"
+  ) {
+    console.log("Valid user");
+  } else {
+    console.log("Invalid user");
+  }
+});
