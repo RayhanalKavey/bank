765a976390b6d03930c9b9d58651b96ce5a1998a
RayhanalKavey <rayhanalkavey@gmail.com>
2022-08-16 20:24:25 +0600

    add event enent handler to the deposite button. When clicked the deposite button the amount will be added to the deposite amount and to the balance amount.

--- js/calculation.js
+++ js/calculation.js
@@ -1,1 +1,41 @@
 "use strict";
+//-----------
+//Get element
+//-----------
+//input amount
+const depositeAmount = document.getElementById("deposite-amount");
+const withdrawAmount = document.getElementById("withdraw-amount");
+//balance
+const calculatedBalance = document.getElementById("calculated-balance");
+//input field
+const depositeInputField = document.getElementById("deposite-input-field");
+const withdrawInputField = document.getElementById("withdraw-input-field");
+// btn
+const btnDeposite = document.getElementById("deposite-btn");
+const btnWithdraw = document.getElementById("withdraw-btn");
+
+//--------------
+//Event listener
+//--------------
+btnDeposite.addEventListener("click", function () {
+  //Add to deposite amount
+  const newDepositeAmount = parseFloat(depositeInputField.value);
+  const previousDepositeAmont = parseFloat(depositeAmount.innerText);
+  const depositeTotal = previousDepositeAmont + newDepositeAmount;
+  depositeAmount.innerText = depositeTotal;
+  depositeInputField.value = "";
+  //Add to balance
+  const previousBalamce = parseFloat(calculatedBalance.innerText);
+  const balanceTotal = previousBalamce + newDepositeAmount;
+  calculatedBalance.innerText = balanceTotal;
+  // if (
+  //   typeof Number(depositeInputField.value) !== "number" ||
+  //   typeof Number(withdrawInputField.value) !== "number"
+  // ) {
+  //   alert("Please input valid amound");
+  // }
+  // console.log(depositeInputField.value);
+  // console.log(withdrawInputField.value);
+  // console.log(typeof depositeAmount);
+});
+console.log();
--- js/login.js
+++ js/login.js
@@ -6,9 +6,9 @@ const passwordField = document.getElementById("user-password");
 // Event Listener
 loginButton.addEventListener("click", function () {
   const emailTyped = emailField.value;
-  console.log(emailTyped);
+  // console.log(emailTyped);
   const passwordTyped = passwordField.value;
-  console.log(passwordTyped);
+  // console.log(passwordTyped);
   // Danger: Do not varify email password on the client site like this. This is only for learning purpose. We usually send it to the server site.
   if (
     emailTyped.toLowerCase() === "personal@bank.com" &&
